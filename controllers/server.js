3426a370c0b9abd82ce422f27921cdd546366dff
Daniel Robertson <danielrobertson@Daniels-MBP15.local>
2016-03-29 18:56:28 -0500

    removed local npm modules, installed them globally

--- controllers/server.js
+++ controllers/server.js
@@ -2,16 +2,16 @@ var express = require('express');
 var app = express();
 
 /**
- define the routes
+ define the api endpoints
  */
-app.post('/api/alerts/unsubscribe/number/:number', function (req, res) {
-    res.send("Unsubscribed " + number);
-    console.log("Unsubscribed " + number);
+app.post('/api/alerts/unsubscribe/number/:number', function (request, response) {
+    res.send("Unsubscribed " + request.params.number);
+    console.log("Unsubscribed " + request.params.number);
 });
 
-app.post('/api/alerts/subscribe/number/:number', function (req, res) {
-    res.send("Subscribed " + number);
-    console.log("Subscribed " + number);
+app.post('/api/alerts/subscribe/number/:number', function (request, response) {
+    res.send("Subscribed " + request.params.number);
+    console.log("Subscribed " + request.params.number);
 });
 
 app.get('/api/alerts/users', function (req, res) {
