d1705f66af637b94d75c950d5b4f3370eec66072
Daniel Robertson <danielrobertson@daniels-mbp15.austin.ibm.com>
2016-03-30 11:23:22 -0500

    authenticated connection to firebase

--- controllers/server.js
+++ controllers/server.js
@@ -1,26 +1,51 @@
-var express = require('express');
-var app = express();
+var Express = require('express');
+var Firebase = require('firebase');
+var FirebaseTokenGenerator = require("firebase-token-generator");
+
+// define web service
+var app = Express();
+
+// connect to database
+var database = new Firebase(process.env.FIREBASE_URL);
+var tokenGenerator = new FirebaseTokenGenerator(process.env.FIREBASE_SECRET);
+var token = tokenGenerator.createToken({uid: "dr"});
+database.authWithCustomToken(token, function (error) {
+    if (error) {
+        console.log("Firebase login failed\n", error);
+    } else {
+        console.log("Firebase login succeeded");
+    }
+});
+
 
 /**
  define the api endpoints
  */
-app.post('/api/alerts/unsubscribe/number/:number', function (request, response) {
-    res.send("Unsubscribed " + request.params.number);
-    console.log("Unsubscribed " + request.params.number);
-});
 
+// subscribe the user
 app.post('/api/alerts/subscribe/number/:number', function (request, response) {
-    res.send("Subscribed " + request.params.number);
+    database.push(request.params.number);
+
+    response.send("Subscribed " + request.params.number);
     console.log("Subscribed " + request.params.number);
 });
 
-app.get('/api/alerts/users', function (req, res) {
-    res.send("Here's all the users");
+// unsubscribe the user
+app.post('/api/alerts/unsubscribe/number/:number', function (request, response) {
+    response.send("Unsubscribed - " + request.params.number);
+    console.log("Unsubscribed - " + request.params.number);
 });
+
+// process the data by sending it to users
+app.get('/api/alerts/process/:data', function (req, res) {
+    res.send("Here's the data to push to users - " + data);
+});
+
 
 /**
  start the server
  */
+
 var server = app.listen(8080, function () {
     var host = server.address().address;
     var port = server.address().port;
